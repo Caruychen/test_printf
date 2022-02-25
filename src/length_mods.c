d53198ef5269d851b3640f0175d650636d16701f
Caruy Chen <cchen@student.hive.fi>
2022-02-25 16:08:51 +0200

    start testing length mods

--- src/length_mods.c
+++ src/length_mods.c
@@ -6,7 +6,7 @@
 /*   By: cchen <cchen@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/22 17:04:37 by cchen             #+#    #+#             */
-/*   Updated: 2022/02/23 08:37:49 by cchen            ###   ########.fr       */
+/*   Updated: 2022/02/25 16:02:04 by cchen            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,25 @@ int	test_flags(void)
 	printf("%hd\n", (short) 42);
 	printf("%ld\n", (long) 42);
 	printf("%lld\n", (long long) 42);
-	specs_one_int("%hhd\n", (char) 42);
-	specs_one_short("%hd\n", (short) 42);
-	specs_one_long("%ld\n", (long) 42);
-	specs_one_long_long("%lld\n", (long long) 42);
+	ft_printf("%hhx\n", (char) 42);
+	ft_printf("%hx\n", (short) 42);
+	ft_printf("%lx\n", (long) 42);
+	ft_printf("%llx\n", (long long) 42);
+	printf("%hhx\n", (char) -42);
+	printf("%hx\n", (short) -42);
+	printf("%lx\n", (long) -42);
+	printf("%llx\n", (long long) -42);
+	ft_printf("%hhx\n", (char) -42);
+	ft_printf("%hx\n", (short) -42);
+	ft_printf("%lx\n", (long) -42);
+	ft_printf("%llx\n", (long long) -42);
+	specs_one_int("%hhx\n", (char) 42);
+	specs_one_short("%hx\n", (short) 42);
+	specs_one_long("%lx\n", (long) 42);
+	specs_one_long_long("%llx\n", (long long) 42);
+	specs_one_int("%hhx\n", (char) -42);
+	specs_one_short("%hx\n", (short) -42);
+	specs_one_long("%lx\n", (long) -42);
+	specs_one_long_long("%llx\n", (long long) -42);
 	return (0);
 }
